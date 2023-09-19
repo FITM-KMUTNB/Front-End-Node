ef7a3a614281d4b1f620a05fb3520e03b9cf34cc
Phatkavee <s6506022630031@kmutnb.ac.th>
2023-09-19 10:45:11 +0700

    ss

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const app = express();
 var bodyParser = require('body-parser');
 
-const base_url = 'https://localhost:3000';
+const base_url = 'http://localhost:3000';
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
@@ -14,17 +14,17 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', async (req, res) => {
     try {
         const response = await axios.get(base_url + '/books');
-        res.render('book', { books: response.data });
+        res.render('books', { books: response.data });
     } catch (err) {
         console.error(err);
         res.status(500).send('Error');
     }
 });
 
-app.get('/book/;id', async (req, res) => {
+app.get('/book/:id', async (req, res) => {
     try {
         const response = await axios.get(base_url + '/books/' + req.params.id);
-        res.render('book', { books: response.data });
+        res.render('book', { book: response.data });
     } catch (err) {
         console.error(err);
         res.status(500).send('Error');
